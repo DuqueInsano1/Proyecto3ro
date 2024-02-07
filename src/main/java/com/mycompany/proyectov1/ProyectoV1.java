69948057914d0d1ba685941cace6b02e2ec30173
Belial <belialwolf39@gmail.com>
2024-02-06 19:14:51 -0500

    Rama master!

---
+++ src/main/java/com/mycompany/proyectov1/ProyectoV1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectov1;
+
+/**
+ *
+ * @author Belial
+ */
+public class ProyectoV1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
